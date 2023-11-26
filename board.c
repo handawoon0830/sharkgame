45c9e4be42345486551e5fb6be2eb977bf1c6198
handawoon0830 <handawoon0830@naver.com>
2023-11-26 16:08:55 +0900

    2

--- board.c
+++ board.c
@@ -5,12 +5,60 @@
 #define BOARDSTATUS_OK		1
 #define BOARDSTATUS_NOK		0
 
+#define N_COINPOS 			12
+#define MAX_COIN			4
+
+#define N_PLAYER 			3
+
+#define PLAYERSTATUS_LIVE	0
+#define PLAYERSTATUS_DIE	1
+#define PLAYERSTATUS_END	2
+
+#define MAX_CHARNAME 		200
+
+#define MAX_SHARKSTEP		6
+#define SHARK_INITPOS		-4
+
+static int player_position[N_PLAYER]; // 위치 
+static char player_name[N_PLAYER][MAX_CHARNAME]; //이름 
+static int player_coin[N_PLAYER]; //누적 코인 
+static int player_status[N_PLAYER]; //현재 상태 
+static char player_statusString[3][MAX_CHARNAME] = {"LIVE","DIE","END"}; //상태설명 문자열 
+
 static int board_status[N_BOARD];
 static int board_coin[N_BOARD];
 
 static int board_shark_position;
 
-int board_initBoard(void);
+static int game_end(void);
+static int getAlivePlayer(void);
+static int getWinner(void);
+
+void board_initBoard(void){
+	int i;
+	for (i=0; i<N_BOARD; i++)
+	{
+		board_status[i]=0;
+		board_coin[i]=0;
+	}
+	
+	srand((unsigned)time(NULL));
+	
+	for (i=0; i<N_COINPOS; i++)
+	{
+		int j;	
+		do{
+			
+			j= 1+ rand()%N_BOARD;
+			if (board_coin[j] != 0 ){
+				board_coin[j] = 1+ rand()%N_COINPOS;
+				break;
+			}
+		}
+		while (board_coin[j] == 0);
+	}
+	
+}
 int board_printBoardStatus(void){
 	int i;
 	printf("----------------------------------- BOARD STATUS-------------------------------------\n");
@@ -26,8 +74,107 @@ int board_printBoardStatus(void){
 	printf("|\n");
 	printf("--------------------------------------------------------------------------------------\n");
 }
-int board_getBoardCoin(int pos);
+int board_getBoardCoin(int pos){
+	int coin;
+	coin = 0;
+	
+	if (board_coin[pos] != 0);
+		coin += board_coin[pos];
+		board_coin[pos] = 0;
+	return coin;
+}
 int board_getSharkPosition(void);
-int board_stepShark(void);
-int board_getBoardStatus(int pos);
+int board_stepShark(void){
+	int pos;
+	pos = 1+ rand()%MAX_SHARKSTEP;
+	int i;
+	for (i=0; i<pos+1; i++)
+	{
+		board_status[i]= BOARDSTATUS_NOK;
+	}
+	shark_position += pos;
+}
+int board_getBoardStatus(int pos){
+	/*
+	if (board_coin[pos] != 0);
+		coin += board_coin[pos];
+		board_coin[pos] = 0;
+	 } 
+	 */
+}
 
+ void printPlayerPosition(int player)
+ {
+	int i;
+	 	
+	for(i=0; i<N_BOARD; i++)
+	{
+	 	printf("|");
+	 		
+	 	if (player_position[player] == i)
+	 		printf("%c", player_name[player][i]);
+	 	else
+	 	{
+	 		if (board_getBoardStatus(i) == BOARDSTATUS_NOK)
+	 			printf("X");
+	 		else
+	 			printf(" ");
+		}
+		 printf("|\n");
+	}
+}
+
+void printPlayerStatus(void)
+{
+	int i;
+	printf("player status ---\n");
+	for (i=0; i<N_PLAYER; i++)
+	{
+		printf("%s : pos %d, coin %d, status %s\n",player_name[i], player_position[i], player_coin[i], player_statusString[player_status[i]]);
+		printPlayerPosition(i);
+	}
+}
+
+
+int game_end(void)
+{
+	int i;
+	int flag_end=1;
+	
+	for (i=0; i<N_PLAYER; i++)
+	{
+		if (player_status[i]== PLAYERSTATUS_LIVE)
+		{
+			flag_end=0;
+			break;
+		}
+	}
+	return flag_end;
+}
+
+int getAlivePlayer(void)
+{
+	int i;
+	int cnt=0;
+	for (i=0; i<N_PLAYER; i++)
+	{
+		if (player_status[i]== PLAYERSTATUS_END)
+			cnt++;
+	}
+	return cnt;
+}
+
+int getWinner(void)
+{
+	int i;
+	int winner=0;
+	int max_coin =-1;
+	for (i=0; i<N_PLAYER; i++){
+		if (player_coin[i]> max_coin)
+		{
+			max_coin = player_coin[i];
+			winner=i;
+		}
+	}
+	return winner;
+}
--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include "board.h"
+#include "board.c"
+
+#define MAX_DIE		6
+ 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int rolldie(void)
 {
@@ -18,40 +22,103 @@ void ending(void){
 	printf("====================================\n");
 }
 
+void checkDie(void){
+	int i;
+	for(i=0; i<N_PLAYER; i++){
+		if (board_status[player_position[i]] == BOARDSTATUS_NOK)
+			player_status[i] == PLAYERSTATUS_DIE;
+			printf("%s in pos %d has died!! (coin %d)", player_name[i], player_position[i], player_coin[i]);
+	}
+}
 int main(int argc, char *argv[]) {
 	srand((unsigned)time(NULL));
 	//0. 오프닝 
 	opening();
 	//1. 초기화 2. 플레이어 이름 결정
-	board_initboard();
+	board_initBoard();
+	
+	int i;
+	for (i=0; i<N_PLAYER; i++){
+		player_position[i]=0;
+		player_coin[i]=0;
+		player_status[i] = PLAYERSTATUS_LIVE;
+		printf("Player %i's name: ", i);
+		scanf("%s", player_name[i]);
+	} 
 	
 	//3. 반복문 돌리기(플레이어 턴을 도는)
 	
+	
+	int turn=0;
+	int flag_end;
+	
 	do{
-		int step= rolldie();
+		board_printBoardStatus();
 		int coinResult;
-		pos += step;
-		print();
-		coinResult = board_getBoardCoin(pos);
+		coinResult = 0;
 		
+		//2-1. 플레이어 상태 출력
+		printPlayerStatus();
+		
+		 //2-2. 주사위 던지기 
+		 int dieResult;
+		 int dum;
+		 //int step= rolldie();
+		 printf("%s turn!!", player_name[turn]);
+		 printf("Press any key to roll a die!\n");
+		 scanf("%d", &dum);
+		 fflush(stdin);
+		 dieResult = rolldie();
+		 
+		 //2-3. 이동
+		//pos += step;
+		player_position[turn] += dieResult;
+			if (player_position[turn]>=N_BOARD-1)
+			{
+				printf("%s reached to the end!!!", player_name[turn]);
+				player_status[turn]= PLAYERSTATUS_END;
+			}
+				
+		printf("Die result : %d, %s moved to %d", dieResult, player_name[turn],player_position[turn]);
+		
+		 //2-4. 동전 줍기
+		int pos= player_position[turn];
+		board_getBoardCoin(pos);
+		coinResult += board_getBoardCoin(pos);
+		player_coin[turn]+= coinResult;
+		if (board_coin[pos] != 0)
+			printf("LUCKY! %s got %d coins!", player_name[turn], board_coin[pos]);
+		
+		
+		//2-5. 다음턴
+		int c;
 		printf("press any key to continue: ");
 		scanf("%d", &c);
 		fflush(stdin);
-	}
-	 //2-1. 플레이어 상태 출력
-	 
-	 //2-2. 주사위 던지기 
-	 
-	 //2-3. 이동
-	 
-	 //2-4. 동전 줍기
-	 
-	 //2-5. 다음턴
-	 
-	 //2-6. if이용 상어의 동작 설정 (조건: 모든 플레이어가 한번씩 턴을 돈다)
-	 	//상어 동작
-	
-	//3. 정리(승자 계상, 출력 등) 
+		
+		if (player_status[turn] != PLAYERSTATUS_END)
+		{
+			turn= (turn +1)%N_PLAYER;
+			continue;
+		}
+		
+		if (turn ==0)
+		{
+			board_stepShark();
+			int shark_pos = board_stepShark();
+			printf("Shark moved to %i\n", shark_pos);
+			checkDie();
+			
+		}
+		
+		game_end();
+		if (flag_end ==1)
+		{
+			getAlivePlayer();
+			getWinner();
+		}
+	} while (flag_end == 1);
+	  
 	ending();
 	return 0;
 	
